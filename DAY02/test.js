a6bbbb835de09b87bd04db70fb09a812a3316018
junhyunsim0402 <simjjang11@gmail.com>
2025-12-30 18:22:27 +0900

    과제
    과제

---
+++ DAY02/test.js
@@ -0,0 +1,48 @@
+let Price = 2000;
+let Calories = 150;
+let Number = 42;
+
+console.log("숫자 자료");
+console.log(Price, Calories, Number);
+
+let storeName = "빽다방 안양역점";
+let drinkName = "빵";
+let welcomeMessage = "맛있게 준비해 드릴게요!";
+
+console.log("문자열 자료");
+console.log(storeName, drinkName, welcomeMessage);
+
+let isSoldOut = false;
+let isTakeOut = true;
+let hasWhipping = false;
+
+console.log("불리언 자료");
+console.log(isSoldOut, isTakeOut, hasWhipping);
+
+let coffeeMenu = ["아메리카노", "카페라떼", "바닐라라떼"];
+let iceOptions = ["각얼음", "간얼음"];
+let selectedItems = ["아메리카노", "빽스치노", "소시지빵"];
+
+console.log("배열 자료");
+console.log(coffeeMenu, iceOptions, selectedItems);
+
+let drinkInfo = {
+    name: "원조커피",
+    price: 2500,
+    isIce: true
+};
+
+let userOrder = {
+    orderID: 2025123001,
+    tableNumber: 5,
+    paymentMethod: "Card"
+};
+
+let storeInfo = {
+    location: "경기도 안양시",
+    openTime: "08:00",
+    closeTime: "22:00"
+};
+
+console.log("객체 자료");
+console.log(drinkInfo, userOrder, storeInfo);
